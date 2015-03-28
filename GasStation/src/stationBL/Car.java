2f40a37a9b90f26e440a382f6a3b601b5215e08e
djnwo <hengoldburd@gmail.com>
2015-03-28 14:09:10 +0300

    .

--- GasStation/src/stationBL/Car.java
+++ GasStation/src/stationBL/Car.java
@@ -1,5 +1,25 @@
 package stationBL;
 
+import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+@XmlRootElement
 public class Car {
+	@XmlAttribute
+	public boolean wantCleaning;
+	
+	@XmlAttribute
+	public int id;
+	
+	@XmlElement(name = "WantsFuel")
+	public WantsFuel wantsfuel;
+	
+	@Override
+	public String toString() {
+		return "Car [wantCleaning=" + wantCleaning + ", id=" + id
+				+ ", wantsfuel=" + wantsfuel + "]";
+	}
 
+	
+    
 }
--- GasStation/src/stationBL/GasStation.java
+++ GasStation/src/stationBL/GasStation.java
@@ -1,8 +1,44 @@
 package stationBL;
 
+import java.util.ArrayList;
 import java.util.LinkedList;
+import java.util.List;
 
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlAnyElement;
+import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlElementWrapper;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement
 public class GasStation {
-	private FuelStock fuelStock;
-	private LinkedList<FuelPump> pumps;
+	@XmlElement
+	public FuelStock MainFuelPool;
+	
+	//public LinkedList<FuelPump> pumps;
+	
+	@XmlElement
+	public WashService CleaningService;
+	
+	@XmlElementWrapper(name = "Cars")
+	@XmlElement(name = "Car")
+	public List<Car> cars=null;
+	
+	@XmlAttribute
+	public int numOfPumps;
+	@XmlAttribute
+	public float pricePerLiter;
+	
+	public GasStation()
+	{
+	}
+
+	public String printCars() {
+		String out="";
+		for(Car c : cars)
+			out+=" "+c.toString();
+		return out;
+	}
 }
